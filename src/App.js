c7ddeb41f907e86cf46e2bc6b8ac01976af2d1e2
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2022-02-13 12:44:02 +0200

    created component layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Controls from "./components/Controls";
+import MusicChanels from "./components/MusicChanels";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MusicChanels />
+      <Controls />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MusicChanels.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MusicChanels() {
+  return <div></div>;
+}
