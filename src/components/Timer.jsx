a118fe0853616d101fe899d95ef18f44fda77a10
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2022-02-15 18:24:43 +0200

    added key to timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,7 +4,7 @@ export default function Timer() {
   const createTimerByDuration = (duration) => {
     const elems = [];
     for (let i = 0; i <= duration; i++) {
-      elems.push(<div>|</div>);
+      elems.push(<div key={i}>|</div>);
     }
     return elems;
   };
